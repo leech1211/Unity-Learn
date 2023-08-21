dfdfc7643084285922bb5f48e2eafc3801cd6c9e
이창훈 <leech1211@nate.com>
2023-08-21 16:20:50 +0900

    1.3 - High Speed Chase

--- Create with Code/Create with code/Assets/Scripts/PlayerController.cs
+++ Create with Code/Create with code/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //transform.Translate(0,0,1);
-        transform.Translate(Vector3.forward * Time.deltaTime * 10);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
