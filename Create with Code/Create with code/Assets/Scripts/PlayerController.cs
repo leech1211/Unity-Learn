35b5fadc0dae8186a66ab13f8a92fe86cdedbc87
이창훈 <leech1211@nate.com>
2023-08-22 11:55:15 +0900

    1.4 - Step into the Driver's Seat

--- Create with Code/Create with code/Assets/Scripts/PlayerController.cs
+++ Create with Code/Create with code/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float speed;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float VerticalInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        turnSpeed = 25;
+        speed = 15;
     }
 
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
         //transform.Translate(0,0,1);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * VerticalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
